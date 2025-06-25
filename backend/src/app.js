efb8fbc851071e33db515a576d1011fb9c1e2810
Ayush021 <12114021@nitkkr.ac.in>
2025-06-25 20:31:12 +0530

    first commit

---
+++ backend/src/app.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+app.use(express.json({ limit: "16kb" })); //is used in an Express.js application to parse incoming JSON request bodies, with a size limit.
+app.use(express.urlencoded({ extended: true, limit: "16kb" })); // Parses incoming requests with URL-encoded payloads, with a size limit.
+app.use(cookieParser()); // Parses cookies attached to the client request object.
+app.use(express.static("public")); // Serves static files from the 'public' directory.
+
+//routes import
+
+//routes declaration
+
+export { app };
